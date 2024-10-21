be8f2de812980aada4ae7d33129a97d62d9f34e1
eduardobnhr <eduardobnhr@gmail.com>
2024-10-21 20:47:11 -0300

    instalando modulos importantes para aplicacao

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  /* app.enableCors();
+  await app.listen(process.env.PORT ?? 3000); */
+  
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  
+  Logger.log(`🚀 aplicaçao rodando na porta: ${PORT}`, 'Bootstrap');
 }
 bootstrap();
